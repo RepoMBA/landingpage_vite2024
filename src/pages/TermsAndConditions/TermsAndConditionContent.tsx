0e309fd2d1829296322cc328a9c314e98c9baeab
mraakash09 <akash.nigam@legittai.com>
2024-06-15 15:02:14 +0530

    updated compoenets

---
+++ src/pages/TermsAndConditions/TermsAndConditionContent.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TermsAndConditionContent() {
+  return <div>content</div>;
+}
---
+++ src/pages/TermsAndConditions/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TermsAndConditionContent from "./TermsAndConditionContent";
+
+export default function TermsAndConditions() {
+  return (
+    <div>
+      <TermsAndConditionContent />
+    </div>
+  );
+}
